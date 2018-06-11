96f6dcf1f89a22c03f8f3c373996ecbbe7683626
Luca de Montis <luca.demo@hotmail.it>
2018-06-11 17:51:20 +0200

    uncommented routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-# Rails.application.routes.draw do
+Rails.application.routes.draw do
 
-#   devise_for :users
-#   root to: 'pages#home'
-#   resources :restaurants do
-#     resources :bookings, only:[:new, :create]
-#   end
-#   get '/my-bookings' to "dashboard#mybookings"
-#   get '/my-restaurants' to "dashboard#myrestaurants"
-#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-# end
+  devise_for :users
+  root to: 'pages#home'
+  resources :restaurants do
+    resources :bookings, only:[:new, :create]
+  end
+  get '/my-bookings' to "dashboard#mybookings"
+  get '/my-restaurants' to "dashboard#myrestaurants"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
