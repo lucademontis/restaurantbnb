95e0a2b7e36ae54c8d7dd73f317255a9ded5d8d9
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-06-12 11:43:50 +0200

    authorization for users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,34 @@
 class RestaurantsController < ApplicationController
   def new
+
+    @restaurant = Restaurant.new
+    # calls for the same method within restaurant_policy
+    authorize @restaurant
+
   end
 
   def index
+    # policy_scope is a method that calls the scope resolve from REst_policy
+     @restaurants = policy_scope(Restaurant).order(created_at: :desc)
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+    # calls for the same method within restaurant_policy
+     authorize @restaurant
   end
 
   def create
+
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    # calls for the same method within restaurant_policy
+    authorize @restaurant
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,4 +39,12 @@ class RestaurantsController < ApplicationController
 
   def destroy
   end
+
+
+    private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :user_id)
+  end
+
 end
---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,39 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    # the resolve method defines the scope
+    def resolve
+      #scope refers to the rest class
+      scope.all
+    end
+  end
+
+
+
+  def create?
+    !@user.nil?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+
+  end
+
+  def edit?
+  end
+
+  def destroy?
+  end
+
+  private
+
+  def is_owner_or_admin?
+    @record.user == @user || @user.admin
+  end
+
+
+
+
+end
