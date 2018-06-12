ba2291b9ba91da9ef4940fd968728a3eb86a26dc
Luca de Montis <luca.demo@hotmail.it>
2018-06-12 13:28:38 +0200

    Created view pages for my-booking and my-restaurants (dashboard controller)

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,14 @@
+class DashboardController < ApplicationController
+
+  skip_after_action :verify_authorized
+
+  def my_bookings
+    # @bookings = policy_scope(Booking).where(user: current_user)
+  end
+
+  def my_restaurants
+    # policy_scope calls solve method that retrieves all restaurants
+    @restaurants = policy_scope(Restaurant).where(user: current_user)
+  end
+
+end
