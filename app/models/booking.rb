4fca3346e43edf6a93a95791e1b8c5beee1acca8
Xavier Ros <x.ros.martinez@gmail.com>
2018-06-13 14:51:04 +0200

    validations for restaurants and bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,18 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  validates :number_of_people, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate  :dates
+  # validates :end_date, presence: true, end_date: { after_or_equal_to:  :start_date}
+
+  def dates
+    if start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    elsif end_date < start_date
+      errors.add(:end_date, "cannot be before start date")
+    end
+  end
 end
