d8df1da9e26ad3f91e823daaf06bad43b2b65c4e
Anna Bendahan <an.bendahan@gmail.com>
2018-06-13 15:27:26 +0200

    done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  def total_price
+    number_of_days = self.end_date - self.start_date
+    n = number_of_days.to_i
+   total_price = self.restaurant.price *  self.number_of_people * n
+  end
+
 end
+
+
+
