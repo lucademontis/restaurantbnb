e2e8a67fa1eecb2a3cc3900754340620e48f0460
Xavier Ros <x.ros.martinez@gmail.com>
2018-06-15 16:29:26 +0200

    changes about the name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   # only asks for authorization unless devise controller, admin, index, my_rest, my_books
   after_action :verify_authorized, except: [:index, :my_restaurants, :my_bookings], unless: :skip_pundit?
   after_action :verify_policy_scoped, only: [:index, :my_restaurants, :my_bookings], unless: :skip_pundit?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -13,6 +14,15 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
   private
 
